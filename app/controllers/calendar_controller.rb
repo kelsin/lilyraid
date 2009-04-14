7888aec14d7e1b5a7596458777ba109d4362cfee
Christopher Giroir <kelsin@valefor.com>
2009-04-13 20:34:32 -0700

    Testing without big timezone area

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,101 +1,101 @@
 class CalendarController < ApplicationController
-    skip_before_filter :authorize, :except => :index
+  skip_before_filter :authorize, :except => :index
 
-    def raids
-        cal = Icalendar::Calendar.new
+  def raids
+    cal = Icalendar::Calendar.new
 
-        cal.custom_property("METHOD", "PUBLISH")
-        cal.custom_property("X-WR-CALNAME", Preference.get_setting("guild") + " Raid Calendar")
-        cal.custom_property("X-WR-CALDESC", "Raid schedule for the " + Preference.get_setting("guild") + " Warcraft Guild")
-        cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
+    cal.custom_property("METHOD", "PUBLISH")
+    cal.custom_property("X-WR-CALNAME", Preference.get_setting("guild") + " Raid Calendar")
+    cal.custom_property("X-WR-CALDESC", "Raid schedule for the " + Preference.get_setting("guild") + " Warcraft Guild")
+    cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
 
-        cal.add_component(get_tz)
-
-        Raid.find(:all, :order => :date).each do |raid|
-            event = Icalendar::Event.new
-            params = { "TZID" => ["America/Los_Angeles"] }
-            if raid.date
-                event.dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
-                event.dtend = raid.date.advance(:hours => 4).strftime("%Y%m%dT%H%M%S")
-            end
-            event.url = raid_url(raid)
-            event.location = "Bronzebeard World of Warcraft Server"
-            event.organizer = raid.account.name if raid.account
-            event.summary = raid.name
-            event.description = raid.note
-            event.klass = "PUBLIC"
-            event.uid = raid.uid
-            cal.add_event(event)
-        end
-
-        send_data(cal.to_ical,
-                  :filename => "raids.ics",
-                  :type => "text/calendar")
+    # cal.add_component(get_tz)
+    
+    Raid.find(:all, :order => :date).each do |raid|
+      if raid.date
+        event = Icalendar::Event.new
+        params = { "TZID" => ["America/Los_Angeles"] }
+        event.dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
+        event.dtend = raid.date.advance(:hours => 4).strftime("%Y%m%dT%H%M%S")
+        event.url = raid_url(raid)
+        event.location = "Bronzebeard World of Warcraft Server"
+        event.organizer = raid.account.name if raid.account
+        event.summary = raid.name
+        event.description = raid.note
+        event.klass = "PUBLIC"
+        event.uid = raid.uid
+        cal.add_event(event)
+      end
     end
 
-    def account
-        cal = Icalendar::Calendar.new
+    send_data(cal.to_ical,
+              :filename => "raids.ics",
+              :type => "text/calendar")
+  end
 
-        @account = Account.find(params[:id])
+  def account
+    cal = Icalendar::Calendar.new
 
-        cal.custom_property("METHOD", "PUBLISH")
-        cal.custom_property("X-WR-CALNAME", Preference.get_setting("guild") + " Raid Calendar for #{@account.name}")
-        cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
-        cal.custom_property("X-WR-CALDESC", "Raid schedule for #{@account.name} of the " + Preference.get_setting("guild") + " Warcraft Guild")
+    @account = Account.find(params[:id])
 
-        cal.add_component(get_tz)
-        
-        @account.all_raid_signups.each do |raid|
-            character = raid.find_signup(@account).character.name
+    cal.custom_property("METHOD", "PUBLISH")
+    cal.custom_property("X-WR-CALNAME", Preference.get_setting("guild") + " Raid Calendar for #{@account.name}")
+    cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
+    cal.custom_property("X-WR-CALDESC", "Raid schedule for #{@account.name} of the " + Preference.get_setting("guild") + " Warcraft Guild")
 
-            status = if raid.find_character(character)
-                         " in the raid."
-                     else
-                         " in the waiting list."
-                     end
+    cal.add_component(get_tz)
+    
+    @account.all_raid_signups.each do |raid|
+      character = raid.find_signup(@account).character.name
 
-            event = Icalendar::Event.new
-            params = { "TZID" => ["America/Los_Angeles"] }
-            if raid.date
-                event.dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
-                event.dtend = raid.date.advance(:hours => 4).strftime("%Y%m%dT%H%M%S")
-            end
-            event.url = raid_url(raid)
-            event.location = "Bronzebeard World of Warcraft Server"
-            event.organizer = raid.account.name if raid.account
-            event.summary = raid.name
-            event.add_attendee "#{character}#{status}"
-            event.description = raid.note
-            event.klass = "PUBLIC"
-            event.uid = raid.uid
-            cal.add_event(event)
-        end
+      status = if raid.find_character(character)
+                 " in the raid."
+               else
+                 " in the waiting list."
+               end
 
-        send_data(cal.to_ical,
-                  :filename => "#{@account.name}.ics",
-                  :type => "text/calendar")
-    end            
+      event = Icalendar::Event.new
+      params = { "TZID" => ["America/Los_Angeles"] }
+      if raid.date
+        event.dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
+        event.dtend = raid.date.advance(:hours => 4).strftime("%Y%m%dT%H%M%S")
+      end
+      event.url = raid_url(raid)
+      event.location = "Bronzebeard World of Warcraft Server"
+      event.organizer = raid.account.name if raid.account
+      event.summary = raid.name
+      event.add_attendee "#{character}#{status}"
+      event.description = raid.note
+      event.klass = "PUBLIC"
+      event.uid = raid.uid
+      cal.add_event(event)
+    end
 
-    private
+    send_data(cal.to_ical,
+              :filename => "#{@account.name}.ics",
+              :type => "text/calendar")
+  end            
 
-    def get_tz
-        tz = Icalendar::Timezone.new
-        tz.tzid = "America/Los_Angeles"
-        tz.custom_property("X-LIC-LOCATION", "America/Los_Angeles")
-        daylight = Icalendar::Daylight.new
-        daylight.tzoffsetfrom = "-0800"
-        daylight.tzoffsetto = "-0700"
-        daylight.tzname = "PDT"
-        daylight.dtstart = "19700308T020000"
-        daylight.add_rrule "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
-        tz.add_component(daylight)
-        stan = Icalendar::Standard.new
-        stan.tzoffsetfrom = "-0700"
-        stan.tzoffsetto = "-0800"
-        stan.tzname = "PST"
-        stan.dtstart = "19701101T020000"
-        stan.add_rrule "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
-        tz.add_component(stan)
-        return tz
-    end        
+  private
+
+  def get_tz
+    tz = Icalendar::Timezone.new
+    tz.tzid = "America/Los_Angeles"
+    tz.custom_property("X-LIC-LOCATION", "America/Los_Angeles")
+    daylight = Icalendar::Daylight.new
+    daylight.tzoffsetfrom = "-0800"
+    daylight.tzoffsetto = "-0700"
+    daylight.tzname = "PDT"
+    daylight.dtstart = "19700308T020000"
+    daylight.add_rrule "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
+    tz.add_component(daylight)
+    stan = Icalendar::Standard.new
+    stan.tzoffsetfrom = "-0700"
+    stan.tzoffsetto = "-0800"
+    stan.tzname = "PST"
+    stan.dtstart = "19701101T020000"
+    stan.add_rrule "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
+    tz.add_component(stan)
+    return tz
+  end        
 end
