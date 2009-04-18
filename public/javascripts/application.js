4d241225245cc125cad6e607c31a1514488501f6
Christopher Giroir <kelsin@valefor.com>
2009-04-18 05:04:19 -0700

    Working on rails 2.3 change and jquery change

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,43 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function choose_char(select) {
+    var selects = $$('select.char_select');
+    for(var i=0; i < selects.length; i++) {
+        if(selects[i] != select && selects[i].value == select.value) {
+            selects[i].value = "";
+        }
+    }
+}
+
+function get_selected_slot() {
+    var id = $('td.slot.selected')[0].id;
+
+    return '' + id.replace(/^td_slot_/,'');
+}
+
+function get_selected_url() {
+    var id = $$('td.selected')[0].id;
+
+    if(id.match(/signup_/)) {
+        return 'from_signup_id=' + id.replace(/^signup_/,'');
+    } else if(id.match(/slot_/)) {
+        return 'from_slot_id=' + id.replace(/^td_slot_/,'');
+    } else {
+        return '';
+    }
+}
+
+function list_display(cclass_name, checked) {
+    eles = $$('tr.' + cclass_name);
+    for(var i=0; i < eles.length; i++) {
+        ele = eles[i];
+        if (checked) {
+            ele.show();
+        } else {
+            ele.hide();
+        }
+    }
+}
+
+
