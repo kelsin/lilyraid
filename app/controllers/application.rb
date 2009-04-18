2a62329c58d5f3808c9bc532c812fa606ea4ffe6
Christopher Giroir <kelsin@valefor.com>
2009-04-18 00:58:39 +0000

    New authorize based on environment

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,45 +4,51 @@
 require 'dbi'
 
 class ApplicationController < ActionController::Base
-    helper :all # include all helpers, all the time
+  helper :all # include all helpers, all the time
 
-    # See ActionController::RequestForgeryProtection for details
-    # Uncomment the :secret if you're not using the cookie session store
-    protect_from_forgery # :secret => '599f44b29c4a78b118884b977571fb10'
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '599f44b29c4a78b118884b977571fb10'
 
-    before_filter :authorize
 
-    private
+  if RAILS_ENV == 'development'
+    before_filter :fake_authorize
+  else
+    before_filter :authorize    
+  end
 
-	def fake_authorize
-        session[:account_id] = 2
-        @current_account = Account.find(2)
-	end
 
-    def authorize
-        # Check to see if we have an account_id in memory
-        if session[:account_id] and session[:account_id].to_i != 1
-            # Get Account info
-            @current_account = Account.get_account_from_id(session[:account_id])
-        else
-            # We don't, redirect
-            redirect_to(:controller => 'login', :action => 'index')
-        end
+  private
+
+  def fake_authorize
+    session[:account_id] = 2
+    @current_account = Account.find(2)
+  end
+
+  def authorize
+    # Check to see if we have an account_id in memory
+    if session[:account_id] and session[:account_id].to_i != 1
+      # Get Account info
+      @current_account = Account.get_account_from_id(session[:account_id])
+    else
+      # We don't, redirect
+      redirect_to(:controller => 'login', :action => 'index')
     end
+  end
 
-    def require_admin
-        # Check if the admin flag is set
-        unless @current_account.admin
-            flash[:error] = "You do not have permission for this action"
-            render raids_url()
-            return false
-        end
+  def require_admin
+    # Check if the admin flag is set
+    unless @current_account.admin
+      flash[:error] = "You do not have permission for this action"
+      render raids_url()
+      return false
     end
-    
-    def display_error(msg)
-        render :update do |page|
-            page.alert(msg)
-            yield page if block_given?
-        end
+  end
+  
+  def display_error(msg)
+    render :update do |page|
+      page.alert(msg)
+      yield page if block_given?
     end
+  end
 end
