3a19fa5e3b062552e9b66b4bbfba24e3866f582d
Christopher Giroir <kelsin@valefor.com>
2009-04-19 15:18:43 -0700

    New inflector for DPS

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,7 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural /^(dps)$/i, '\1'
+end
