1a83256fdeca56b491d2357dcf61f05d770ae2ec
Christopher Giroir <kelsin@valefor.com>
2009-04-21 15:48:35 -0700

    Precaution for bad raids

--- db/migrate/20090421224227_fix_utc_times.rb
+++ db/migrate/20090421224227_fix_utc_times.rb
@@ -1,15 +1,19 @@
 class FixUtcTimes < ActiveRecord::Migration
   def self.up
     Raid.all.each do |raid|
-      raid.date = raid.date + 7.hours
-      raid.save
+      if raid.date
+        raid.date = raid.date + 7.hours
+        raid.save
+      end
     end
   end
 
   def self.down
     Raid.all.each do |raid|
-      raid.date = raid.date - 7.hours
-      raid.save
+      if raid.date
+        raid.date = raid.date - 7.hours
+        raid.save
+      end
     end
   end
 end
