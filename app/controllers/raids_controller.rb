232bf5245a73f6b0ed438f7ada7c1f90a2c5bf27
Christopher Giroir <kelsin@valefor.com>
2009-04-21 17:31:39 -0700

    Date time picker works!

--- app/controllers/raids_controller.rb
+++ app/controllers/raids_controller.rb
@@ -1,119 +1,122 @@
 class RaidsController < ApplicationController
-    before_filter(:load_raid, :only => [:edit, :update, :destroy, :show, :finalize])
+  before_filter(:load_raid, :only => [:edit, :update, :destroy, :show, :finalize])
 
-    def index
-        @old = params[:old]
+  def index
+    @old = params[:old]
 
-        if @old
-            @raids = Raid.find(:all,
-                               :conditions => ["raids.date < ?", Time.now - 5.hours],
-                               :order => "raids.date desc")
-        else
-            @raids = Raid.find(:all,
-                               :conditions => ["raids.date >= ?", Time.now - 5.hours],
-                               :order => "raids.date")
-        end
+    if @old
+      @raids = Raid.find(:all,
+                         :conditions => ["raids.date < ?", Time.now - 5.hours],
+                         :order => "raids.date desc")
+    else
+      @raids = Raid.find(:all,
+                         :conditions => ["raids.date >= ?", Time.now - 5.hours],
+                         :order => "raids.date")
+    end
+  end
+
+  def show
+    if @raid.uses_loot_system
+      @list = List.first
+    end
+  end
+
+  def finalize
+    if @current_account == @raid.account or @current_account.admin
+      @raid.finalized = !@raid.finalized
+      @raid.save
     end
 
-    def show
-      if @raid.uses_loot_system
-        @list = List.first
+    respond_to do |format|
+      format.html { redirect_to raid_url(@raid) }
+    end
+  end
+
+  def edit
+    if @current_account == @raid.account or @current_account.admin
+      @roles = Role.find(:all)
+      @cclasses = Cclass.find(:all)
+      
+      respond_to do |format|
+        format.html
+      end
+    else
+      flash[:error] = "You are not authorized to edit this account"
+      respond_to do |format|
+        format.html { redirect_to raid_url(@raid) }
       end
     end
+  end
 
-    def finalize
-        if @current_account == @raid.account or @current_account.admin
-            @raid.finalized = !@raid.finalized
-            @raid.save
-        end
+  def update
+    if @current_account.can_edit(@raid)
+      @raid.update_attributes(params[:raid])
+      @raid.date = Time.parse("#{params[:caldate]} #{params[:caltime]}")
+      @raid.save
 
-        respond_to do |format|
-            format.html { redirect_to raid_url(@raid) }
-        end
+      # Update Slots
+
+      Slot.update(params[:slot].keys, params[:slot].values)
+
+      respond_to do |format|
+        format.html { redirect_to raid_url(@raid) }
+      end
     end
+  end
 
-    def edit
-        if @current_account == @raid.account or @current_account.admin
-            @roles = Role.find(:all)
-            @cclasses = Cclass.find(:all)
-            
-            respond_to do |format|
-                format.html
-            end
-        else
-            flash[:error] = "You are not authorized to edit this account"
-            respond_to do |format|
-                format.html { redirect_to raid_url(@raid) }
-            end
-        end
+
+  def destroy
+    if @current_account.can_edit(@raid)
+      @raid.destroy
+
+      respond_to do |format|
+        format.html { redirect_to raids_url }
+        format.js
+      end
+    else
+      flash[:error] = "You don't have permission to delete that raid"
+      respond_to do |format|
+        format.html { redirect_to raids_url }
+        format.js { redirect_to raids_url }
+      end
+    end        
+  end
+
+  def instance_levels
+    instance = Instance.find(params[:id])
+    
+    render :update do |page|
+      page[:raid_min_level].value = instance.min_level
+      page[:raid_max_level].value = instance.max_level
+      page[:level_loading].replace_html ""
     end
+  end
 
-    def update
-        if @current_account.can_edit(@raid)
-            @raid.update_attributes(params[:raid])
+  def new
+    @raid = Raid.new
+    @raid.date = Date.tomorrow + 18.hours unless @raid.date
+  end
 
-            # Update Slots
+  def create
+    # Create raid object
+    @raid = Raid.new(params[:raid])
+    @raid.account = @current_account
+    @raid.date = Time.parse("#{params[:caldate]} #{params[:caltime]}")
 
-            Slot.update(params[:slot].keys, params[:slot].values)
-
-            respond_to do |format|
-                format.html { redirect_to raid_url(@raid) }
-            end
-        end
+    if @raid.save
+      flash[:notice] = 'Raid saved!'
+      redirect_to raid_url(@raid)
+    else
+      @instances = Instance.find(:all, :order => "name")
+      @raid_templates = RaidTemplate.find(:all)
+      render :action => 'new'
     end
+  end
 
+  private
 
-    def destroy
-        if @current_account.can_edit(@raid)
-            @raid.destroy
-
-            respond_to do |format|
-                format.html { redirect_to raids_url }
-                format.js
-            end
-        else
-            flash[:error] = "You don't have permission to delete that raid"
-            respond_to do |format|
-                format.html { redirect_to raids_url }
-                format.js { redirect_to raids_url }
-            end
-        end        
-    end
-
-    def instance_levels
-        instance = Instance.find(params[:id])
-        
-        render :update do |page|
-            page[:raid_min_level].value = instance.min_level
-            page[:raid_max_level].value = instance.max_level
-            page[:level_loading].replace_html ""
-        end
-    end
-
-    def new
-        @raid = Raid.new
-        @raid.date = Time.now unless @raid.date
-    end
-
-    def create
-        # Create raid object
-        @raid = Raid.new(params[:raid])
-        @raid.account = @current_account
-        
-        if @raid.save
-            flash[:notice] = 'Raid saved!'
-            redirect_to raid_url(@raid)
-        else
-            @instances = Instance.find(:all, :order => "name")
-            @raid_templates = RaidTemplate.find(:all)
-            render :action => 'new'
-        end
-    end
-
-    private
-
-    def load_raid
-        @raid = Raid.find(params[:id])
-    end
+  def load_raid
+    @raid = Raid.find(params[:id])
+  end
 end
 
