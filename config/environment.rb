65ea176cd95ef5a6ac6b05a1e8f1193c6dc8000c
Christopher Giroir <kelsin@valefor.com>
2010-02-25 17:39:01 -0800

    New hoptoad gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "dbi"
   config.gem "mysql"
   config.gem "icalendar"
+  config.gem "hoptoad_notifier"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
