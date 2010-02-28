f547ade358b95bb753e34c6bce6a52347c4230a6
Christopher Giroir <kelsin@valefor.com>
2010-02-28 16:27:31 -0500

    Seed data for races and classes

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# Classes
+[{ :name => 'Death Knight', :color => 'c41f3b' },
+ { :name => 'Druid', :color => 'ff7d0a' },
+ { :name => 'Mage', :color => '69ccf0' },
+ { :name => 'Paladin', :color => 'f58cba' },
+ { :name => 'Priest', :color => 'ffffff' },
+ { :name => 'Rogue', :color => 'fff569' },
+ { :name => 'Shaman', :color => '2459ff' },
+ { :name => 'Hunter', :color => 'abd473' },
+ { :name => 'Warlock', :color => '9482ca' },
+ { :name => 'Warrior', :color => 'c79c6e' }].each do |class_attributes|
+  Cclass.create(class_attributes)
+end
+
+# Races
+['Blood Elf', 'Draenei', 'Dwarf', 'Gnome', 'Human', 'Night Elf', 'Orc', 'Tauren', 'Troll', 'Undead'].each do |name|
+  Race.create(:name => name)
+end
