269b45457a4f84a492cdfa71a38ae6a7d66400b2
Christopher Giroir <kelsin@valefor.com>
2010-03-10 20:58:49 -0500

    Added named scopes for tags

--- app/models/raider_tag.rb
+++ app/models/raider_tag.rb
@@ -2,4 +2,8 @@ class RaiderTag < ActiveRecord::Base
   belongs_to :tag
   belongs_to :raid
   belongs_to :account
+
+  named_scope :in_past, lambda { |time| { :include => :raid, :conditions => ['raids.date >= ?', Time.now - time] } }
+  named_scope :for_raid, lambda { |raid| { :conditions => { :raid_id => raid } } }
+  named_scope :for_account, lambda { |account| { :conditions => { :account_id => account } } }
 end
