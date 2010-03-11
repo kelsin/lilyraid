533a3a031ae296c4c65f57dd52917a2be187ac1f
Christopher Giroir <kelsin@valefor.com>
2010-03-10 21:14:04 -0500

    Added associations

--- app/models/raider_tag.rb
+++ app/models/raider_tag.rb
@@ -5,5 +5,6 @@ class RaiderTag < ActiveRecord::Base
 
   named_scope :in_past, lambda { |time| { :include => :raid, :conditions => ['raids.date >= ?', Time.now - time] } }
   named_scope :for_raid, lambda { |raid| { :conditions => { :raid_id => raid } } }
+  named_scope :for_tag, lambda { |tag| { :conditions => { :tag_id => tag } } }
   named_scope :for_account, lambda { |account| { :conditions => { :account_id => account } } }
 end
