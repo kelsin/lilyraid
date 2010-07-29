177379ca92d554b82d1af5642c77730ae45db410
Christopher Giroir <kelsin@valefor.com>
2010-07-29 15:05:02 -0400

    Better formatting for icalendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,23 +1,19 @@
 class CalendarController < ApplicationController
   skip_before_filter :authorize, :except => :index
+  skip_before_filter :fake_authorize, :except => :index
+
+  before_filter :load_calendar, :only => [:raids, :account]
 
   def raids
-    cal = Icalendar::Calendar.new
-
-    cal.custom_property("METHOD", "PUBLISH")
-    cal.custom_property("X-WR-CALNAME", CONFIG[:guild] + " Raid Calendar")
-    cal.custom_property("X-WR-CALDESC", "Raid schedule for the " + CONFIG[:guild] + " Warcraft Guild")
-    cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
-
-    # cal.add_component(get_tz)
+    @cal.custom_property("X-WR-CALDESC", "Raid schedule for the " + CONFIG[:guild] + " Warcraft Guild")
 
     Raid.find(:all, :order => :date).each do |raid|
       if raid.date
-        params = { "TZID" => ["America/Los_Angeles"] }
         rurl = raid_url(raid)
-        cal.event do
-          dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
-          duration 'PT4H'
+        @cal.event do
+          dtstart raid.date.strftime("%Y%m%dT%H%M%S")
+          dtend raid.date.advance(:hours => 3).strftime("%Y%m%dT%H%M%S")
+          duration 'PT3H'
           url rurl
           location "Bronzebeard World of Warcraft Server"
           organizer raid.account.name if raid.account
@@ -29,30 +25,24 @@ class CalendarController < ApplicationController
       end
     end
 
-    send_data(cal.to_ical,
+    send_data(@cal.to_ical,
               :filename => "raids.ics",
               :type => "text/calendar")
   end
 
   def account
-    cal = Icalendar::Calendar.new
-
     @account = Account.find(params[:id])
+    @cal.custom_property("X-WR-CALDESC", "Raid schedule for #{@account.name} of the " + CONFIG[:guild] + " Warcraft Guild")
 
-    cal.custom_property("METHOD", "PUBLISH")
-    cal.custom_property("X-WR-CALNAME", CONFIG[:guild] + " Raid Calendar for #{@account.name}")
-    cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
-    cal.custom_property("X-WR-CALDESC", "Raid schedule for #{@account.name} of the " + CONFIG[:guild] + " Warcraft Guild")
-
-    cal.add_component(get_tz)
-
-    @account.all_raid_signups.each do |raid|
+    @account.signups.map do |signup|
+      signup.raid
+    end.each do |raid|
       if raid.date
-        params = { "TZID" => ["America/Los_Angeles"] }
         rurl = raid_url(raid)
-        cal.event do
-          dtstart raid.date.strftime("%Y%m%dT%H%M%S"), params
-          duration 'PT4H'
+        @cal.event do
+          dtstart raid.date.strftime("%Y%m%dT%H%M%S")
+          dtend raid.date.advance(:hours => 3).strftime("%Y%m%dT%H%M%S")
+          duration 'PT3H'
           url rurl
           location "Bronzebeard World of Warcraft Server"
           organizer raid.account.name if raid.account
@@ -64,31 +54,37 @@ class CalendarController < ApplicationController
       end
     end
 
-    send_data(cal.to_ical,
+    send_data(@cal.to_ical,
               :filename => "#{@account.name}.ics",
               :type => "text/calendar")
   end
 
   private
 
-  def get_tz
-    tz = Icalendar::Timezone.new
-    tz.tzid = "America/Los_Angeles"
-    tz.custom_property("X-LIC-LOCATION", "America/Los_Angeles")
-    daylight = Icalendar::Daylight.new
-    daylight.tzoffsetfrom = "-0800"
-    daylight.tzoffsetto = "-0700"
-    daylight.tzname = "PDT"
-    daylight.dtstart = "19700308T020000"
-    daylight.add_rrule "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
-    tz.add_component(daylight)
-    stan = Icalendar::Standard.new
-    stan.tzoffsetfrom = "-0700"
-    stan.tzoffsetto = "-0800"
-    stan.tzname = "PST"
-    stan.dtstart = "19701101T020000"
-    stan.add_rrule "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
-    tz.add_component(stan)
-    return tz
+  def load_calendar
+    @cal = Icalendar::Calendar.new
+    @cal.custom_property("METHOD", "PUBLISH")
+    @cal.custom_property("X-WR-CALNAME", CONFIG[:guild] + " Raid Calendar")
+    @cal.custom_property("X-WR-TIMEZONE", "America/Los_Angeles")
+
+    @cal.timezone do
+      timezone_id             "America/Los_Angeles"
+
+      daylight do
+        timezone_offset_from  "-0800"
+        timezone_offset_to    "-0700"
+        timezone_name         "PDT"
+        dtstart               "19700308TO20000"
+        add_recurrence_rule   "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
+      end
+
+      standard do
+        timezone_offset_from  "-0700"
+        timezone_offset_to    "-0800"
+        timezone_name         "PST"
+        dtstart               "19701101T020000"
+        add_recurrence_rule   "YEARLY;BYMONTH=11;BYDAY=1SU"
+      end
+    end
   end
 end
